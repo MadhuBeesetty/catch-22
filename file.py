b3f579ee947406ff061451e6e16723272f351242
madhusudhanrao beesetty <madhusudhan@madhusudhan.local>
2017-12-09 12:13:12 -0800

    pox controler code

---
+++ file.py
@@ -0,0 +1,28 @@
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        leftHost1 = self.addHost( 'h2' )
+        rightHost = self.addHost( 'h3' )
+        rightHost1 = self.addHost( 'h4' )
+        leftSwitch = self.addSwitch( 's3' )
+        rightSwitch = self.addSwitch( 's4' )
+
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftHost1, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
+        self.addLink( rightSwitch, rightHost1 )
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
+
